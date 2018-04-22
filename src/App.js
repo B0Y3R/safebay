895388c3970deb51eab57391ca375f77dc8a6435
James Boyer <jamesboyer@Jamess-MacBook-Pro.local>
2018-04-22 01:10:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <header className="App-header">
+         <h1>header</h1>
+        </header>
+
+        <div className="Main">
+        
+        <div className="left-nav">
+          <h1>Hey</h1>
+        </div>
+
+        <div className="Center">
+          <h1>Hi</h1>
+        </div>
+
+        <div className="Aside">
+          <h1>Hello</h1>
+        </div>
+        </div>
+        
+        <footer>
+          <h1>Footer</h1>
+        </footer>
+
+        
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
