f72c4cf661b8bd267bb1d90c64d0e832937cb42a
James Boyer <jamesboyer@Jamess-MacBook-Pro.local>
2018-04-22 20:52:29 -0400

    attempting to put logo in center main content section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Components/Header';
 import Main from './Components/MainContent/Main';
 import Footer from './Components/Footer';
+import './logo.svg';
 import './App.css';
 
 class App extends Component {
